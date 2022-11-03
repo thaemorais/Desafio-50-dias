9a2c800d5db41096ed8f1980c935dc7450cec4e1
thaemorais <thaelen.lima@edu.ufes.br>
2022-11-03 09:28:00 -0300

    Dia 1/50

---
+++ dia1.py
@@ -0,0 +1,8 @@
+#Dia 1: Um programa que entre o preço de um produto e o valor em dinheiro pago. Imprima para o usuário o quanto será o troco.
+
+preco = float(input())
+pago = float(input())
+
+troco = pago - preco
+
+print(troco)
